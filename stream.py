1aa8bb88815def2eee425e65d769b5399214d97a
Jimmy <ritter.jimmy@gmail.com>
2024-05-12 09:00:16 +0100

    handling app stop and refresh

--- stream.py
+++ stream.py
@@ -1,20 +1,21 @@
 from flask import Flask, Response
 from picamera2 import Picamera2
-import io
 import cv2
+import signal
+import sys
 
 app = Flask(__name__)
+picam2 = Picamera2()
 
-def gen_frames():
-    picam2 = Picamera2()
+def init_camera():
     picam2.start_preview()  # Start the preview to configure the camera
     preview_config = picam2.create_preview_configuration(main={"size": (640, 480)})
     picam2.configure(preview_config)
-
     picam2.start()
 
+def gen_frames():
     while True:
-        buffer = picam2.capture_array()  # Capture frame as a numpy array
+        buffer = picam2.capture_array()
         ret, jpeg = cv2.imencode('.jpg', buffer)  # Convert frame to JPEG
         frame = jpeg.tobytes()
         yield (b'--frame\r\n'
@@ -24,5 +25,14 @@ def gen_frames():
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+def signal_handler(sig, frame):
+    print('You pressed Ctrl+C!')
+    if picam2:
+        picam2.stop()
+    sys.exit(0)
+
 if __name__ == '__main__':
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+    init_camera()
     app.run(host='0.0.0.0', port=5000)
