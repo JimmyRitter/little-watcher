07834f90e99264f77c0327c429c66fb26db11bdf
Jimmy <ritter.jimmy@gmail.com>
2024-05-16 20:06:36 +0100

    including kinesis video stream on terraform file

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,20 +13,25 @@ provider "aws" {
   region  = "eu-west-1"
 }
 
+# S3
 resource "aws_s3_bucket" "recordings_bucket" {
   bucket = local.envs["AWS_S3_RECORDINGS_BUCKET"]
   force_destroy = true
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
 }
 
+# SES
 resource "aws_ses_email_identity" "sender_email" {
   email = local.envs["AWS_SES_EMAIL_SOURCE_ADDRESS"]
 }
 
 resource "aws_ses_email_identity" "recipient_email" {
   email = local.envs["AWS_SES_EMAIL_RECIPIENT_ADDRESS"]
+}
+
+# # Kinesis Video Stream
+# resource "aws_kinesis_video_stream" "video_stream" {
+#   name                    = "kinesis-video-stream"
+#   data_retention_in_hours = 1
+#   device_name             = "raspberry-pi"
+#   # media_type              = "video/h264"
 }
